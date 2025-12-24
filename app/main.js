9f76e90a456c0645471293e944b13082b7f69e46
Oladunjoye <shonubij@gmail.com>
2025-12-24 14:17:00 -0800

    add log

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
+ rl.question("$ ", (answer) => {
   console.log(`${answer}: command not found`)
   rl.close();
-});
+ });
