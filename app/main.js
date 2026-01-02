9aab272aba78ef3ada16c6c21119cd79b4f8c48c
Oladunjoye <shonubij@gmail.com>
2026-01-01 16:56:33 -0800

    locate executable v1

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const readline = require("readline");
+const fs = require('fs')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,16 +9,58 @@ const rl = readline.createInterface({
 rl.setPrompt('$ ')
 
 const validCommands =[ 'echo', 'type', 'exit']
+
+function validatePathExistsAndIsExecutable(path){
+  try {
+    fs.accessSync(path, fs.constants.X_OK)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+// PATH="/usr/bin:/usr/local/bin:$PATH" ./your_program.sh
+async function validatePathType(command){
+  const {PATH:pathEnv} = process.env
+
+  if (!pathEnv){
+    return false
+  }
+
+
+  const splitPath = pathEnv.split(':')
+  for (path of splitPath){
+    const pathExistsAndIsExecutable =  validatePathExistsAndIsExecutable(path)
+    if(pathExistsAndIsExecutable){
+      console.log(`${command} is ${path}/${command}`)
+      return true
+    }
+  }
+
+  return false
+
+}
+
 function handleTypeInput(input){
 if (validCommands.includes(input)){
   console.log(`${input} is a shell builtin`)
 }
 else{
-  console.log(`${input}: not found`)
+
+  const validPath = validatePathType(input)
+
+  if(validPath){
+    console.log(`${input} is ${validPath}`)
+  }else{
+
+    console.log(`${input}: not found`)
+  }
 
 }
 
+return
+
 }
+
 function processInput(input){
   if(input === 'exit'){
     rl.close()
@@ -39,4 +82,15 @@ function processInput(input){
 rl.prompt()
 rl.on('line', processInput)
 
+
+
+// path  = /dir1:/dir2:/dir3 -> splits paths into /dir, /dir2. /dir3
+
+// when yoy use type:
+// - check if the inpourt is a valid builtin, , send genericic built in mesasge and return/
+// - if notDeepEqual, check all the paths in the every directory provided, 
+// if the file exist, check the execuratiable permissions
+// if file exists and had executable perms, then print, command is full path
+// else skip and contunye
+// last case not found
 
